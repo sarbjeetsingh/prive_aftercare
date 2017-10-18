c4267bdd24504ec8c6beb54e266bd56088d29f0e
prabhjotkaur10 <prabhjot@addvalsolutions.com>
2017-10-18 16:05:25 +0530

    prive_aftercare_changes

--- js/contact_me.js
+++ js/contact_me.js
@@ -97,16 +97,21 @@ $('#name').focus(function() {
       });
 // });
 
-
-$(window).scroll(function(){
+function resize(){
   var top_offset = $(window).scrollTop();
+  var windowWidth = $(window).width();
+  console.log(top_offset);
     if (top_offset >= 100){
-      $('.about_wrap').addClass('left-1 slideInLeft');
-      $('.mission_wrap').addClass('right-1 slideInRight');
+      $('.about_wrap').addClass('slideInLeft');
+      $('.mission_wrap').addClass('slideInRight');
+      // $('.about_wrap').addClass('left-1 slideInLeft');
+      // $('.mission_wrap').addClass('right-1 slideInRight');
     }
     else{
-      $('.about_wrap').removeClass('left-1 slideInLeft');
-      $('.mission_wrap').removeClass('right-1 slideInRight');
+      $('.about_wrap').removeClass('slideInLeft');
+      $('.mission_wrap').removeClass('slideInRight');
+      // $('.about_wrap').removeClass('left-1 left-14 slideInLeft');
+      // $('.mission_wrap').removeClass('right-1 right-14 slideInRight');
     }
 
     if(top_offset >1020){
@@ -122,18 +127,92 @@ $(window).scroll(function(){
       $('.right_box').addClass('right-1 slideInRight');
     }
     else{
-      // $('.left_box').removeClass('left-1 slideInLeft');
-      // $('.right_box').removeClass('right-1 slideInRight');
+      $('.left_box').removeClass('left-1 slideInLeft');
+      $('.right_box').removeClass('right-1 slideInRight');
     }
 
     if (top_offset >= 1950){
-      $('.room_type_box .inner_box').addClass('zoomIn').removeClass('hidden');
-      $('.large_room, .small_room').addClass('full_opacity');
+      $('.room_type_box').addClass('full_opacity');
+      // $('.large_room, .small_room').addClass('full_opacity');
     }
     else{
-      $('.room_type_box .inner_box').removeClass('zoomIn').addClass('hidden');
-      $('.large_room, .small_room').removeClass('full_opacity');
+      $('.room_type_box').removeClass('full_opacity');
+      // $('.large_room, .small_room').removeClass('full_opacity');
     }
+}
+
+$(window).scroll(function(){
+  resize();
 });
 
-function init_map(){var myOptions = {zoom:12,center:new google.maps.LatLng(34.0731665,-118.3745462),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(34.0731665,-118.3745462)});infowindow = new google.maps.InfoWindow({content:'<strong>Prive Aftercare</strong><br>8438 West 3rd Street, Los Angeles, CA<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+$("#l_show_more").on("click", function(e){
+  e.preventDefault();
+  console.log('aks');
+  if($('#l_detail').hasClass('hide')){
+    $('#l_detail').removeClass('hide').addClass('fadeIn');
+    $(this).text("Less");
+  }else{
+    $('#l_detail').addClass('hide').removeClass('fadeIn');
+    $(this).text("Learn More");
+  }
+});
+
+$("#s_show_more").on("click", function(e){
+  e.preventDefault();
+  console.log('aks');
+  if($('#s_detail').hasClass('hide')){
+    $('#s_detail').removeClass('hide').addClass('fadeIn');
+    $(this).text("Less");
+  }else{
+    $('#s_detail').addClass('hide').removeClass('fadeIn');
+    $(this).text("Learn More");
+  }
+});
+
+$('#request_booking_btn').on("click", function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+  }, 2000);
+  $('#message').val("I'd like to talk about booking a stay!");
+});
+
+$('#videoPlayerBtn').on('click', function(e){
+  e.preventDefault();
+  $('#videoModal').modal('show');
+  var video = document.getElementById("video_player");
+  $('source',  video).attr('src', 'http://privecare.com/video/Prive-Luxury-Recovery-720p.mp4');
+  video.load();
+  video.play();
+});
+
+$('#close_btn').on('click', function(){
+  $('#videoModal').modal('hide');
+  var video = document.getElementById("video_player");
+  $('source',  video).attr('src', '');
+  video.pause();
+});
+
+$('#sendMessageButton').on('click', function(){
+  Tawk_API.toggle();
+});
+
+// $(document).ready(function(){
+//     /* Get iframe src attribute value i.e. YouTube video url
+//     and store it in a variable */
+//     var url = $("#cartoonVideo").attr('src');
+
+//     /* Assign empty url value to the iframe src attribute when
+//     modal hide, which stop the video playing */
+//     $("#videoModal").on('hide.bs.modal', function(){
+//         $("#cartoonVideo").attr('src', '');
+//     });
+
+//     /* Assign the initially stored url back to the iframe src
+//     attribute when modal is displayed again */
+//     $("#videoModal").on('show.bs.modal', function(){
+//         $("#cartoonVideo").attr('src', url);
+//     });
+// });
+
+function init_map(){var myOptions = {zoom:16,center:new google.maps.LatLng(34.0731665,-118.3745462),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(34.0731665,-118.3745462)});infowindow = new google.maps.InfoWindow({content:'<strong>Prive Aftercare</strong><br>8438 West 3rd Street, Los Angeles, CA<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
